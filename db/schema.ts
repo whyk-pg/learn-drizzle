fb012ef6c64d5b7d602821490c325d4e0bfd7760
WhyK <windchimeyk@gmail.com>
2024-10-27 23:30:08 +0900

    style: biome check

--- db/schema.ts
+++ db/schema.ts
@@ -20,13 +20,17 @@ export const movieTable = mysqlTable("tbl_movieinfo", {
   /** 実写かどうか */
   is_live_action: boolean("is_live_action").notNull(),
   /** 上映館テーブルID */
-  theater_id: int("theater_id").notNull().references(() => theaterTable.id),
+  theater_id: int("theater_id")
+    .notNull()
+    .references(() => theaterTable.id),
   /** 上映開始日時 */
-  view_start_datetime: datetime("view_start_datetime", { mode: "string" })
-    .notNull(),
+  view_start_datetime: datetime("view_start_datetime", {
+    mode: "string",
+  }).notNull(),
   /** 上映終了日時 */
-  view_end_datetime: datetime("view_end_datetime", { mode: "string" })
-    .notNull(),
+  view_end_datetime: datetime("view_end_datetime", {
+    mode: "string",
+  }).notNull(),
   /** 同伴者数 */
   accompanier: int("accompanier"),
   /** 5段階評価 */
